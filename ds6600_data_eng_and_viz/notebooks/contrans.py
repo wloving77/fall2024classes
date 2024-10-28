2ae492df79295cff357e10819e9a5c94c31531a1
Will Loving <wloving77@gmail.com>
2024-10-28 11:20:10 -0400

    got contrans up to date

--- ds6600_data_eng_and_viz/notebooks/contrans.py
+++ ds6600_data_eng_and_viz/notebooks/contrans.py
@@ -3,14 +3,37 @@ import requests
 import pandas as pd
 import os
 from dotenv import load_dotenv
+import BeautifulSoup
 
 
 class contrans:
 
     def __init__(self):
         load_dotenv("../.env")
+        self.mypassword = os.getenv("PASSWORD")
         self.congress_key = os.getenv("CONGRESS_API_KEY")
         self.news_key = os.getenv("NEWS_API_KEY")
+        self.us_state_to_abbrev = {
+                        "Alabama": "AL","Alaska": "AK","Arizona": "AZ","Arkansas": "AR",
+                        "California": "CA","Colorado": "CO","Connecticut": "CT","Delaware": "DE",
+                        "Florida": "FL","Georgia": "GA","Hawaii": "HI",
+                        "Idaho": "ID","Illinois": "IL","Indiana": "IN","Iowa": "IA",
+                        "Kansas": "KS","Kentucky": "KY","Louisiana": "LA",
+                        "Maine": "ME","Maryland": "MD","Massachusetts": "MA",
+                        "Michigan": "MI","Minnesota": "MN","Mississippi": "MS",
+                        "Missouri": "MO","Montana": "MT","Nebraska": "NE",
+                        "Nevada": "NV","New Hampshire": "NH","New Jersey": "NJ",
+                        "New Mexico": "NM","New York": "NY","North Carolina": "NC",
+                        "North Dakota": "ND","Ohio": "OH","Oklahoma": "OK",
+                        "Oregon": "OR","Pennsylvania": "PA","Rhode Island": "RI",
+                        "South Carolina": "SC","South Dakota": "SD","Tennessee": "TN",
+                        "Texas": "TX","Utah": "UT","Vermont": "VT",
+                        "Virginia": "VA","Washington": "WA","West Virginia": "WV",
+                        "Wisconsin": "WI","Wyoming": "WY","District of Columbia": "DC",
+                        "American Samoa": "AS","Guam": "GU","Northern Mariana Islands": "MP",
+                        "Puerto Rico": "PR","United States Minor Outlying Islands": "UM",
+                        "Virgin Islands": "VI"
+                        }
 
     def get_votes(self):
         url = "https://voteview.com/static/data/out/votes/H118_votes.csv"
@@ -61,48 +84,109 @@ class contrans:
         return bio_df
 
 
-    def get_bioguide(self, first_name, last_name, state=None, district=None):
+    def get_bioguide(self, name, state=None, district=None):
         
-        members = self.get_bioguideIDs() # to be stored in PostgreSQL 
+        members = self.get_bioguideIDs() # replace with SQL query
 
-        # Create first and last names field
-        members['last_names'] = members['name'].str.replace(",","").str.lower().str.strip().str.split().str[0]
-        members['first_names'] = members['name'].str.replace(",","").str.lower().str.strip().str.split().str[1]
+        members['name'] = members['name'].str.lower().str.strip()
+        name = name.lower().strip()
 
-        members = members[(members['first_names'] == first_name) & (members['last_names'] == last_name)].copy()
-        
+        tokeep = [name in x for x in members['name']]
+        members = members[tokeep]
+
         if state is not None:
-            members = members[members['state'] == state].copy()
+                members = members.query('state == @state')
 
         if district is not None:
-            members = members[members['district'] == district].copy()
-
-        return members
-    
-    def get_sponsored_legislation(self, bioguideid):
-        root = "https://api.congress.gov/v3/"   
-        endpoint = f'/member/{bioguideid}/sponsored-legislation'
-        headers = self.make_headers()
-        params = {"api_key":self.congress_key, "limit": 1}
-
-        r = requests.get(root + endpoint,
-                         params=params,
-                         headers=headers)
-
-        total_records = r.json()['pagination']['count']        
+                members = members.query('district == @district')
         
-        j=0
-        sl_df = pd.DataFrame()
-        while j < total_records: 
+        return members.reset_index(drop=True)
+
+    def get_sponsoredlegislation(self, bioguideid):
+
+            params = {'api_key': self.congresskey,
+                        'limit': 1} 
+            headers = self.make_headers()
+            root = 'https://api.congress.gov/v3'
+            endpoint = f'/member/{bioguideid}/sponsored-legislation'
+            r = requests.get(root + endpoint,
+                                params=params,
+                                headers=headers)
+            totalrecords = r.json()['pagination']['count']
+            
             params['limit'] = 250
-            params['offset'] = j
-            r = requests.get(root + endpoint, params=params, headers=headers)
-            records = pd.json_normalize((r.json()['sponsoredLegislation']))
-            sl_df = pd.concat([sl_df, records], ignore_index=True)
-            j = j + 250
-        
-        return sl_df
+            j = 0
+            bills_list = []
+            while j < totalrecords:
+                    params['offset'] = j
+                    r = requests.get(root + endpoint,
+                                        params=params,
+                                        headers=headers)
+                    records = r.json()['sponsoredLegislation']
+                    bills_list = bills_list + records
+                    j = j + 250
 
+            return bills_list
+            
+    def make_cand_table(self, members):
+            # members is output of get_terms()
+            replace_map = {'Republican': 'R','Democratic': 'D','Independent': 'I'}
+            members['partyletter'] = members['partyName'].replace(replace_map)
+            members['state'] = members['state'].replace(self.us_state_to_abbrev)
+            members['district'] = members['district'].fillna(0)
+            members['district'] = members['district'].astype('int').astype('str')
+            members['district'] = ['0' + x if len(x) == 1 else x for x in members['district']]
+            members['district'] = [x.replace('00', 'S') for x in members['district']]
+            members['DistIDRunFor'] = members['state']+members['district']
+            members['lastname']= [x.split(',')[0] for x in members['name']]
+            members['firstname']= [x.split(',')[1] for x in members['name']]
+            members['name2'] = [ y.strip() + ' (' + z.strip() + ')' 
+                            for y, z in 
+                            zip(members['lastname'], members['partyletter'])]
+            
+            cands = pd.read_csv('data/CampaignFin22/cands22.txt', quotechar="|", header=None)
+            cands.columns = ['Cycle', 'FECCandID', 'CID','FirstLastP',
+                            'Party','DistIDRunFor','DistIDCurr',
+                            'CurrCand','CycleCand','CRPICO','RecipCode','NoPacs']
+            cands['DistIDRunFor'] = [x.replace('S0', 'S') for x in cands['DistIDRunFor']]
+            cands['DistIDRunFor'] = [x.replace('S1', 'S') for x in cands['DistIDRunFor']]
+            cands['DistIDRunFor'] = [x.replace('S2', 'S') for x in cands['DistIDRunFor']]
+            cands['name2'] = [' '.join(x.split(' ')[-2:]) for x in cands['FirstLastP']]
+            cands = cands[['CID', 'name2', 'DistIDRunFor']].drop_duplicates(subset=['name2', 'DistIDRunFor'])
+            crosswalk = pd.merge(members, cands, 
+                    left_on=['name2', 'DistIDRunFor'],
+                    right_on=['name2', 'DistIDRunFor'],
+                    how = 'left')
+            return crosswalk
+
+    def terms_df(self, members):
+            termsDF = pd.DataFrame()
+            for index, row in members.iterrows():
+                    bioguide_id = row['bioguideId']
+                    terms = row['terms.item']
+                    df = pd.DataFrame.from_records(terms)
+                    df['bioguideId'] = bioguide_id
+                    termsDF = pd.concat([termsDF, df])
+            members = members.drop('terms.item', axis=1)
+            return termsDF, members
+
+    ### Methods for building the 3NF relational DB tables
+
+    def make_members_df(self, members, ideology):
+            members_df = pd.merge(members, ideology, 
+                                    left_on='bioguideId', 
+                                    right_on='bioguide_id',
+                                    how='left')
+            return members_df
+
+    def make_terms_df(self):
+            return self
+
+    def make_votes_df(self):
+            return self
+
+    def make_agreement_df(self):
+            return self
 
     # NEWS API:
 
@@ -118,6 +202,10 @@ class contrans:
             return r.json()
         else:
             return "Error Processing Request"
+
+
+    def make_cand_table(self): 
+        return -1
         
 
         
